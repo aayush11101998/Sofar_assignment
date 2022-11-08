1d143fe0338b67a563f404b9ced699e801ffa015
aayush11101998 <91724060+aayush11101998@users.noreply.github.com>
2022-11-08 15:27:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 colcon build
 source install/local_setup.bash
-World_Models = $"sofar/sofar_assignment/src/multirobots_gazebo/world/models"
-Turtlebot_models = $"opt/ros/galactic/share/turtlebot3_gazebo/models"
+World_Models = $(pwd)"src/multirobots_gazebo/world/models"
+Turtlebot_models = $(pwd)"src/multirobots_gazebo/resource"
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$Turtlebot_models:$World_Models
 export TURTLEBOT3_MODEL=waffle
 ros2 launch two_robot_nav multirobot_launch.py 
